a35628df8bbd33d383bd0c65e52b10f641d00bf2
ChristyTropila <tropila04@gmail.com>
2020-09-29 02:34:12 -0400

    updated the name for the board

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ leftCard.className="card-info"
 let mainObj={}
 let globalNav=undefined
 let itemId={}
-let globalCollection={}
+let globalCollectionId={}
 let rightSide=document.getElementById('side-bar')
-//rightSide.style.display="none";
+rightSide.style.display="none";
 
 let loginPopup=document.getElementsByClassName('login-form-popup')
 let signupPopup=document.getElementsByClassName('signup-form-popup')
@@ -229,17 +229,28 @@ fetch('http://localhost:5000/categories')
                          
                             categName.innerText=item.name
                             buttonAndItem.append(categName)
-
                             rightSide.append(buttonAndItem)
+
+
                             let deleteButton=document.createElement('BUTTON')
                             deleteButton.type="submit"
-                            deleteButton.innerText="REMOVE"
+                            deleteButton.innerText="Remove"
                             buttonAndItem.append(deleteButton)
                             rightSide.append(buttonAndItem)
+
+                            let updateButton=document.createElement("BUTTON")
+                             updateButton.type="submit"
+                             updateButton.innerHTML="Update Name"
+                             buttonAndItem.append(updateButton)
+                             rightSide.append(buttonAndItem)
+
+
                              let getCard=document.getElementsByClassName('card')
-                            mainContainer.id="main-container-2"
-                             rightSide.style.display="block"
+                              mainContainer.id="main-container-2"
+                              rightSide.style.display="block"
                               getCard.className="card-2"
+
+
                             deleteButton.addEventListener('click', (evt) => {
                                  fetch(`http://localhost:5000/collection_boards/${collection.id}`, {
                                     method: 'DELETE'
@@ -251,16 +262,71 @@ fetch('http://localhost:5000/categories')
                             
                            })
                         
+
+                           updateButton.addEventListener("click", (evt) => {
+                             evt.preventDefault()
+                             globalCollectionId=collection.id
+                             mainContainer.innerHTML=""
+                             let updateform=document.createElement('form')
+                             
+                            updateform.className="form-container"
+                             let heading=document.createElement('h3')
+                             heading.innerText="Update Collection Name!"
+                              let inputField=document.createElement('input')
+                              inputField.placeholder="Update Collection Name"
+                              inputField.type="text"
+                      
+                             let updateButton=document.createElement('BUTTON')           
+                               updateButton.className="btn"
+                              updateButton.type="submit"
+                             updateButton.innerText="Update!"
+                             updateform.append(heading, inputField, updateButton)
+                            mainContainer.append(updateform)
+                             
+                               updateform.addEventListener("submit", (evt) => {
+                                   evt.preventDefault()
+                                   let newName=document.querySelector('input').value
+                                   console.log(newName)
+                                   fetch(`http://localhost:5000/collection_boards/${collection.id}`, {
+                                       method: 'PATCH',
+                                       headers: {
+                                        'Content-Type': 'application/json',
+                                        Accept: 'application/json'
+                                     },
+                                     body: JSON.stringify({
+                                         name: newName,
+                                         id: collection.id,
+                                         item_id: renderItemId(collection)
+                                       
+                                
+                                   })
+
+                               })
+                               .then(resp => resp.json())
+                               .then(collection => {
+                                   console.log(collection)
+                               })
+                            
+                           })
+
                 
+
                })
           })
                         
      })
                      
- }
+ })
 
  }
+}
 
+
+let renderItemId=(collection)=> {
+    collection.items.forEach (item => {
+        item.id
+    })
+}
  checkToSeeIfCollectionExist()
                 
             
