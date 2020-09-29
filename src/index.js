352cf6e4aee90f6e629b7fb44adc2b4fc16d0a50
ChristyTropila <tropila04@gmail.com>
2020-09-29 14:06:12 -0400

    register form

--- src/index.js
+++ src/index.js
@@ -10,49 +10,83 @@ let mainHeading=document.createElement('h2')
 mainHeading.className="side-label"
 let leftCard=document.createElement('div')
 leftCard.className="card-info"
+
+let rightSide=document.getElementById('side-bar')
+rightSide.style.display="none";
+
+
 let mainObj={}
 let globalNav=undefined
 let itemId={}
 let globalCollectionId={}
-let rightSide=document.getElementById('side-bar')
-rightSide.style.display="none";
+let currentUser={}
 
-let loginPopup=document.getElementsByClassName('login-form-popup')
-let signupPopup=document.getElementsByClassName('signup-form-popup')
-let loginForm=document.getElementById('login-form')
-let signupForm=document.getElementById('signup-form')
-let loginBtn=document.getElementById('login-submit')
-let signupBtn=document.getElementById('signup-submit')
+// let loginPopup=document.getElementsByClassName('login-form-popup')
+// let signupPopup=document.getElementsByClassName('signup-form-popup')
+// let loginForm=document.getElementById('login-form')
+// let signupForm=document.getElementById('signup-form')
+// let loginBtn=document.getElementById('login-submit')
+// let signupBtn=document.getElementById('signup-submit')
+
+ let signUPAction=() => {
+   let signUpPopup=document.createElement('div')
+   signUpPopup.className="signup-form-popup"
+   let signupForm=document.createElement('form')
+   signupForm.className="form-container"
+   signupForm.id="signup-form"
+   let label=document.createElement('h2')
+   label.innerText="Signup"
+   let nameInput=document.createElement('input')
+   nameInput.placeholder="Name"
+   nameInput.name="name"
+   nameInput.type="text"
+   let usernameInput=document.createElement('input')
+   usernameInput.placeholder="User Name"
+   usernameInput.name="username"
+   usernameInput.type="text"
+   let createAcct=document.createElement("BUTTON")
+   createAcct.type="submit"
+   createAcct.class="login-btn"
+   createAcct.id="login-submit"
+   signupForm.append(label, nameInput, usernameInput, createAcct)
+   signUpPopup.append(signupForm)
+   mainContainer.append(signUpPopup)
 
 
-// // html.append(loginPopup, signupPopup)
-
-// loginPopup.hide()
-//  let signUPAction=() => {
-//      signupForm.addEventListener("submit", (e) => {
-//         e.preventDefault()
-//        let getName=e.target.name.value
-//        let userName=e.target.username.value
-//         fetch('http://localhost:5000/users', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Accept: 'application/json'
-//              },
-//              body: JSON.stringify({
-//                  name: getName,
-//                  username:userName
-//              })
-//         })
-//         e.target.reset() 
+   signupForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+       let getName=e.target.name.value
+       let userName=e.target.username.value
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+             },
+             body: JSON.stringify({
+                 name: getName,
+                 username:userName
+             })
+        })
+        .then(resp=>resp.json())
+        .then(response => {
+           currentUser=response  
+            if(response.id){
+              startMainPage()
+            }
+        })
+     
         
-//      })
-//  }
-//  signUPAction()
+     })
+ }
+ signUPAction()
 
 
 
 
+ function startMainPage(){
+    mainContainer.innerHTML=""
+    //this will hide the collection board when the user first enters page
 let checkToSeeIfCollectionExist=()=>{
     fetch('http://localhost:5000/collection_boards')
     .then(resp=> resp.json())
@@ -69,132 +103,125 @@ let checkToSeeIfCollectionExist=()=>{
 
 
    
-
-
-
 //create li for nav bar
 //append to container
 //create click event
-fetch('http://localhost:5000/categories')
-.then(resp => resp.json())
-.then(categoryArray => {
-    
-    mainCategory(categoryArray[0])
-    mainObj=categoryArray
-     categoryArray.forEach((cat)=> {
-         renderCategoryList(cat)
-     
-     })
-
-    })
-
-    let getCollectionNames=() => {
-        fetch('http://localhost:5000/collection_boards')
-        .then(resp=>resp.json())
-        .then(collections => {
-                collections.forEach(collect => {
-                    
-                })
-      
+    fetch('http://localhost:5000/categories')
+    .then(resp => resp.json())
+    .then(categoryArray => {
+        
+        //display the first category on the main section
+        mainCategory(categoryArray[0])
+        mainObj=categoryArray
+        categoryArray.forEach((cat)=> {
+            renderCategoryList(cat)
+        
         })
-    }
+        
+        let sweater3=document.createElement('img')
+        sweater3.src="styles/images/sweater1.png"
+        sweater3.className="sweater-2"
+        let sweaters=document.createElement('img')
+        sweaters.src="styles/images/sweater2.png"
+        let sweater2=document.createElement('img')
+        sweater2.className="sweater"
+        sweater2.src="styles/images/sweater3.png"
+        navBarUl.append(sweaters, sweater2, sweater3)
+        navBarSection(navBarUl)
+        })
 
     //create category elements and append
- let renderCategoryList= (cat) => {
-     navButton=document.createElement('li')
-     navButton.innerText=cat.name
-     navBarUl.append(navButton)
-     navBarSection.append(navBarUl)
-     globalNav=navButton
-
+    let renderCategoryList= (cat) => {
+    
+        navButton=document.createElement('li')
+        navButton.innerText=cat.name
+        navBarUl.append(navButton)
+        navBarSection.append(navBarUl)
+        globalNav=navButton
+        navBarSection.append(navBarUl)
    
      navButton.addEventListener("click",(evt) => {
-        
-   
          mainCategory(cat)
          
          })
      }
 
    
-     //display item in main container
+     //display items in main container
  let mainCategory=(category) =>{
-    mainContainer.innerHTML=""
-  
-
-    // mainHeading.innerText=category.name
-    // mainContainer.append(mainHeading)
-    title=document.createElement("h2")
-          category.items.forEach(item => {
-            itemId=item.id
-             mainContainer.HTML=""
-            let cardDiv=document.createElement('div')
-            cardDiv.className="card"
-             title=document.createElement("h4")
-             descrip=document.createElement('p')
-             address=document.createElement('p')
-             url=document.createElement('a')
-            url.setAttribute('href', item.url)
-            url.textContent="Website Link"
-            url.target="_blank"
-           image=document.createElement('img')
-            image.className="image-top"
-            image.src=item.image
-            image.width=200
-            image.height=250
-            title.innerText=item.name
-            descrip.innerText=item.description
-            address.innerText=item.location
-              cardDiv.append(image)
-              mainContainer.append(cardDiv)
-              cardDiv.append(title)
-              mainContainer.append(cardDiv)
-              cardDiv.append(descrip)
-              mainContainer.append(cardDiv)
-              cardDiv.append(address)
-              mainContainer.append(cardDiv)
-              cardDiv.append(url)
-              mainContainer.append(cardDiv)
-        
+        mainContainer.innerHTML=""
+        title=document.createElement("h2")
+            category.items.forEach(item => {
+                itemId=item.id
+                mainContainer.HTML=""
+                let cardDiv=document.createElement('div')
+                cardDiv.className="card"
+                title=document.createElement("h4")
+                descrip=document.createElement('p')
+                address=document.createElement('p')
+                url=document.createElement('a')
+                url.setAttribute('href', item.url)
+                url.textContent="Website Link"
+                url.target="_blank"
+                image=document.createElement('img')
+                image.className="image-top"
+                image.src=item.image
+                image.width=200
+                image.height=250
+                title.innerText=item.name
+                descrip.innerText=item.description
+                address.innerText=item.location
+                cardDiv.append(image)
+                mainContainer.append(cardDiv)
+                cardDiv.append(title)
+                mainContainer.append(cardDiv)
+                cardDiv.append(descrip)
+                mainContainer.append(cardDiv)
+                cardDiv.append(address)
+                mainContainer.append(cardDiv)
+                cardDiv.append(url)
+                mainContainer.append(cardDiv)
+            
         
                let button=document.createElement('span')
                     button.innerText="Pin This Item!"
                     cardDiv.append(button)
                     cardDiv.addEventListener("click", (e) =>{
-                            
                     renderForm(item)
              })
-            })
+          })
 
 
-
-            //form to create a new collection_board
+ //form to create a new collection_board
      let renderForm=(item)=> {
          itemId=item.id
-          mainContainer.innerHTML=""
+         mainContainer.innerHTML=""
+
          let form=document.createElement('form')
-         
-        form.className="form-container"
+          form.className="form-container"
+
          let heading=document.createElement('h3')
          heading.innerText="Create New Collection"
+
           let inputField=document.createElement('input')
           inputField.placeholder="Name Your Collection"
           inputField.type="text"
   
          let submitButton=document.createElement('BUTTON')           
-           submitButton.className="btn"
+        submitButton.className="btn"
           submitButton.type="submit"
          submitButton.innerText="Create!"
+
          form.append(heading, inputField, submitButton)
-                   
-         mainContainer.append(form)
-                
+          mainContainer.append(form)
+              
+          //create collection board form
          form.addEventListener("submit", (evt) => {
                  evt.preventDefault()
                  let name=document.querySelector('input').value
                 
                  fetch('http://localhost:5000/collection_boards', {
-                     method:'POST', 
+                     method:'POST',
                      headers: {
                         'Content-Type': 'application/json',
                         Accept: 'application/json'
@@ -223,7 +250,7 @@ fetch('http://localhost:5000/categories')
                          rightSide.append(buttonAndItem)
                       
                          item.collection_board_id=collection.id
-                      collection.items.forEach(item => {
+                         collection.items.forEach(item => {
                         let categName=document.createElement('h5')
                         categName.className="categ-name"
                          
@@ -250,87 +277,84 @@ fetch('http://localhost:5000/categories')
                               rightSide.style.display="block"
                               getCard.className="card-2"
 
-
+                               //delete collection baord
                             deleteButton.addEventListener('click', (evt) => {
                                  fetch(`http://localhost:5000/collection_boards/${collection.id}`, {
                                     method: 'DELETE'
                                 })
                                 buttonAndItem.remove()
-                          
-                            //    rightSide.display.style="none"
-                            
                             
                            })
-                        
-
-                           updateButton.addEventListener("click", (evt) => {
-                             evt.preventDefault()
-                             globalCollectionId=collection.id
-                             mainContainer.innerHTML=""
-                             let updateform=document.createElement('form')
-                             
-                            updateform.className="form-container"
-                             let heading=document.createElement('h3')
-                             heading.innerText="Update Collection Name!"
-                              let inputField=document.createElement('input')
-                              inputField.placeholder="Update Collection Name"
-                              inputField.type="text"
+                    
+//update collection name form 
+    updateButton.addEventListener("click", (evt) => {
+        evt.preventDefault()
+        globalCollectionId=collection.id
+        mainContainer.innerHTML=""
+        let updateform=document.createElement('form')
+        
+        updateform.className="form-container"
+        let heading=document.createElement('h3')
+        heading.innerText="Update Collection Name!"
+        let inputField=document.createElement('input')
+        inputField.placeholder="Update Collection Name"
+        inputField.type="text"
                       
-                             let updateButton=document.createElement('BUTTON')           
-                               updateButton.className="btn"
-                              updateButton.type="submit"
-                             updateButton.innerText="Update!"
-                             updateform.append(heading, inputField, updateButton)
-                            mainContainer.append(updateform)
+        let updateButton=document.createElement('BUTTON')           
+        updateButton.className="btn"
+        updateButton.type="submit"
+        updateButton.innerText="Update!"
+        updateform.append(heading, inputField, updateButton)
+        mainContainer.append(updateform)
                              
-                               updateform.addEventListener("submit", (evt) => {
-                                   evt.preventDefault()
-                                   let newName=document.querySelector('input').value
-                                   console.log(newName)
-                                   fetch(`http://localhost:5000/collection_boards/${collection.id}`, {
-                                       method: 'PATCH',
-                                       headers: {
-                                        'Content-Type': 'application/json',
-                                        Accept: 'application/json'
-                                     },
-                                     body: JSON.stringify({
-                                         name: newName,
-                                         id: collection.id,
-                                         item_id: renderItemId(collection)
-                                       
-                                
-                                   })
-
-                               })
-                               .then(resp => resp.json())
-                               .then(collection => {
-                                mainCategory(mainObj[0])
-                                  sideLabel.innerText=collection.name
-                               })
-                            
-                           })
-
+        updateform.addEventListener("submit", (evt) => {
+            evt.preventDefault()
+            let newName=document.querySelector('input').value
+            console.log(newName)
+            fetch(`http://localhost:5000/collection_boards/${collection.id}`, {
+                method: 'PATCH',
+                headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+                },
+                body: JSON.stringify({
+                    name: newName,
+                    id: collection.id,
+                    item_id: renderItemId(collection)
                 
+        
+            })
 
-               })
+        })
+                .then(resp => resp.json())
+                .then(collection => {
+                mainCategory(mainObj[0])
+                    sideLabel.innerText=collection.name
+                })
+    
+              })
+
+            })
           })
                         
      })
                      
  })
 
- }
+}
 }
 
+          //helper method to get item_id
+            let renderItemId=(collection)=> {
+                collection.items.forEach (item => {
+                    item.id
+                })
+            }
 
-let renderItemId=(collection)=> {
-    collection.items.forEach (item => {
-        item.id
-    })
-}
+            
  checkToSeeIfCollectionExist()
                 
-            
+}    
 
       
    
